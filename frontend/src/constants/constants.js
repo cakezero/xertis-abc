5b8c969095a01bbe108f3b6bae5ef6b7527be3bb
cakezero <sareahj.com@gmail.com>
2025-04-27 03:45:59 +0100

    basic const config

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,9 @@
+export const API = "https://xertis.onrender.com";
+export const CLIENT = "";
+
+export const symbols = {
+  "award": "AWARD",
+  "completion": "COMPLN",
+  "participation": "PARTN",
+  "others": "OTHER"
+}
