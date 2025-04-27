e5e9883cd3b7f787e79acf516c7755f1da863c96
cakezero <sareahj.com@gmail.com>
2025-04-27 04:20:11 +0100

    custom types

---
+++ server/src/types/types.ts
@@ -0,0 +1,35 @@
+import type { Request } from "express";
+import nodemailer from "nodemailer"
+
+export interface CustomRequest extends Request { 
+  user?: object | string
+}
+
+export type User = {
+  email: String | undefined,
+  walletAddress: String | undefined,
+}
+
+export interface MailOptions extends nodemailer.SendMailOptions {
+  template?: string;
+  context?: {
+    [key: string]: any;
+  };
+}
+
+export type Options = {
+  amount: number,
+  txUrl: string
+}
+
+export interface emailUser {
+  username: string;
+  email: string;
+  certUrl?: string;
+  minter?: string;
+  minted?: number;
+  certificateName?: string;
+  ETHEquivalent?: number;
+  mintPrice?: number;
+  totalAmount?: number;
+}
