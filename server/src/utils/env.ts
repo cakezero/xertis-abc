29867171e746762fb26de47d7986d35eb9749eeb
cakezero <sareahj.com@gmail.com>
2025-04-27 04:20:41 +0100

    env setup for email sender

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -7,3 +7,9 @@ export const PORT = process.env.PORT || 4600;
 export const DB_URL = process.env.DB_URL!;
 
 export const ENVIRONMENT = process.env.ENVIRONMENT || "development";
+
+export const EMAIL_SERVICE = process.env.EMAIL_SERVICE || "gmail";
+
+export const EMAIL_USER = process.env.EMAIL_USER!;
+
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD!;
