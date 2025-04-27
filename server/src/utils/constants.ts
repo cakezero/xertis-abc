79d9c6823255b9ace6af9864f565dbf777c8d3bf
cakezero <sareahj.com@gmail.com>
2025-04-27 20:31:48 +0100

    ipfs certificate images hashes

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,3 +1,10 @@
 export const BLOCK_EXPLORER = "https://testnet.snowtrace.io";
 
 export const ETHPrice = 1; // todo: get avax price thorugh api fetch
+
+export const images = {
+  award: "ipfs://bafkreicfy3rz5yo7qqcsmsosj2i3amfvrbts62q4gtl4dwk5722d4jmkni",
+  completion: "ipfs://bafkreiaxl7hzzwgwznzntm6rf276zhdwgty2ej7zh6y2a2dj2ntfwidm4a",
+  participation: "ipfs://bafkreicccux3n2xndb2jxv4kacpg5tqlnzupo5hzmox5yvpefexe6bbd4a",
+  others: "ipfs://bafkreiavk6gr7dc6ojyq5deruhc4mknhwjvagncao2phlloicy4xc5nxgq"
+};
