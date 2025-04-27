c4338f81c5166f26a827f63b69d73effa0a52d22
cakezero <sareahj.com@gmail.com>
2025-04-27 20:23:26 +0100

    certificate and creator routes api routes

--- server/src/routes/certRoutes.ts
+++ server/src/routes/certRoutes.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import { certificateInfo, createMetadata, createCert, getRevenue, mintedCert } from "../controllers/cert"
 
 const router = express.Router();
 
+router
+  .get("/get-revenue", getRevenue)
+  .post("/create-cert", createCert)
+  .post("/mint-cert", mintedCert)
+  .post("/get-metadata-url", createMetadata)
+  .get("/:certId", certificateInfo);
+  
 export default router;
--- server/src/routes/creatorRoutes.ts
+++ server/src/routes/creatorRoutes.ts
@@ -1,5 +1,29 @@
 import express from "express";
+import {
+  createEmail,
+  mintedEmail,
+  creatorRegister,
+  history,
+  creatorLogin,
+  getRevenue,
+  getWallet,
+  setWallet,
+  fetchAssets,
+  withdrawEmail
+} from "../controllers/creator"; 
 
 const router = express.Router();
+
+router
+  .get("/history", history)
+  .get("/get-revenue", getRevenue)
+  .get("/get-assets", fetchAssets)
+  .get("/get-wallet", getWallet)
+  .post("/create-email", createEmail)
+  .post("/withdraw-email", withdrawEmail)
+  .post("/minted", mintedEmail)
+  .post("/set-wallet", setWallet)
+  .post("/register", creatorRegister)
+  .post("/login", creatorLogin)
 
 export default router;
